3f56585cf63c4861a478db3acb3e642818b2afd7
Marcus Dahl <marcus.dahl@everyday.no>
2023-02-01 17:37:15 +0100

    fix: update docs

--- src/config.php
+++ src/config.php
@@ -12,14 +12,16 @@ return [
     'secret' => null,
 
     // Additional events we should trigger updates for. Return an associative array with the event as a key and a function taking the event's signature.
-    // Return an array of URIs to trigger updates for.
-    /* Syntax:
+    // Return an array of [uri, siteHandle] tuples to trigger updates for.
+    /* If you wanted to add support for Retour's AFTER_SAVE_REDIRECT event you'd do the following:
         [
-            \craft\elements\Entry::EVENT_AFTER_SAVE => function (Entry $entry) {
-                // Return URIs we should trigger updates for
-                return [$entry->uri];
+            'class'   => Redirects::class,
+            'event'   => Redirects::EVENT_AFTER_SAVE_REDIRECT,
+            'handler' => static function (RedirectEvent $event) {
+                $sites = $event->siteId ? [Site::find()->where(['id' => $event->siteId])->one()] : Site::find()->all();
+                return array_map(static fn($site) => [$event->legacyUrl, $site->handle], $sites);
             },
-        ]
+        ],
     */
     'additionalEvents' => [],
 
